1476b136b1db7c36030dd0f96533b015e098d6d7
small_sunshine <jioo0224@naver.com>
2017-10-16 19:35:15 +0900

    닉네임 검색방식 변경, 일부 수정

--- router/minecraft.js
+++ router/minecraft.js
@@ -9,13 +9,22 @@ router.get('/', (req, res) => {
 router.get('/nick', (req, res) => {
     res.render('minecraft/nick.pug');
 });
+router.get('/nick/:query', (req, res) => {
+    res.render('minecraft/nick.pug', {query: req.params.query});
+});
 
 router.get('/uuid', (req, res) => {
     res.render('minecraft/uuid.pug');
 });
+router.get('/uuid/:query', (req, res) => {
+    res.render('minecraft/uuid.pug', {query: req.params.query});
+});
 
 router.get('/history', (req, res) => {
     res.render('minecraft/history.pug');
+});
+router.get('/history/:query', (req, res) => {
+    res.render('minecraft/history.pug', {query: req.params.query});
 });
 
 router.get('/api', (req, res) => {
